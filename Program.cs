5c715254a09c66130d5b36030d4825ef95a6c24f
viktormilanov08-png <viktormilanov08@gmail.com>
2026-04-23 21:27:47 +0300

    new version

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using SchoolLabApp.View;
+
+namespace SchoolLabApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new UserLoanPanel());
+        }
+    }
+}
---
+++ View/TechnicianPasswordPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SchoolLabApp.View
+{
+    public partial class TechnicianPasswordPanel : Form
+    {
+        public TechnicianPasswordPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
