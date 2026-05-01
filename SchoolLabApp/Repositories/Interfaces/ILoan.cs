61c53a64b7a97aa4eb3760c4ba88d7d673516c60
Ventsislav <161476611+Venzi22@users.noreply.github.com>
2026-05-01 17:35:33 +0300

    adding asset, loan and Category repository

---
+++ SchoolLabApp/Repositories/Interfaces/ILoan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLabApp.Repositories.Interfaces
+{
+    public interface ILoan<T> : IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetByAssetIdAsync(int assetId);
+
+        Task<IEnumerable<T>> GetByPersonIdAsync(int personId);
+
+        Task<IEnumerable<T>> GetActiveLoansAsync();
+
+        Task<IEnumerable<T>> GetOverdueLoansAsync();
+    }
+
+}
