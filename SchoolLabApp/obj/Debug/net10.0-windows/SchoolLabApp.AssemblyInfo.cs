a22a8f8f4d266d381c1a409ffe7d219da1a71087
Alexvtoriq <agurov582@gmail.com>
2026-05-10 18:34:54 +0300

    f
    f

--- SchoolLabApp/obj/Debug/net10.0-windows/SchoolLabApp.AssemblyInfo.cs
+++ SchoolLabApp/obj/Debug/net10.0-windows/SchoolLabApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolLabApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f6c2a3c213a754d859f89c1dcb39c3b8d2b00fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b6294d8128ae71927266209655700d619728ef7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolLabApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolLabApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
